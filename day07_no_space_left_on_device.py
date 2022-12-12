91833bb936c57557751d92da7796fbfaa52810ed
amikami102 <asakomikami@gmail.com>
2022-12-12 12:05:02 -0500

    Upload day 7 solution; upload day 8 solution to part 1

--- day07_no_space_left_on_device.py
+++ day07_no_space_left_on_device.py
@@ -1,8 +1,92 @@
 """
-Find all of the directories with a total size of at most 100000.
+Within the terminal output, lines that begin with $ are commands you executed, very much like some modern computers:
+
+cd means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
+    cd x moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
+    cd .. moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
+    cd / switches the current directory to the outermost directory, /.
+ls means list. It prints out all files and directories immediately contained by the current directory:
+    123 abc means that the current directory contains a file named abc with size 123.
+    dir xyz means that the current directory contains a directory named xyz.
+
+Find all directories with a total size of at most 100000.
 What is the sum of the total sizes of those directories?
 """
+from collections import defaultdict
+import pprint
+import re
 
+cd_pattern = re.compile('^\$ cd (?P<dir>\S+)$')
+ls_pattern = re.compile('^\$ ls$')
+file_pattern = re.compile('^(?P<filesize>\d+) (?P<filename>\S+)$')
+dir_pattern = re.compile('^dir (?P<dir>\w+)$')
+max_size = 100000
+
+directory_sizes = defaultdict(int)
+path = []
+
+# Build a dictionary of directory paths
 with open('day07_input.txt', 'r') as f:
     for line in f:
-        if line.startswith('$'):
+        if line.startswith('$ cd'):
+            dir = re.match(cd_pattern, line).group('dir')
+            if dir == '/':
+                path = ['/']
+            elif dir == '..':
+                path.pop()
+            else:
+                path.append(dir)
+        elif line.startswith('$ ls'):
+            continue
+        else:
+            if line.startswith('dir'):
+                continue
+            else:
+                file = re.match(file_pattern, line)
+                filesize = int(file.group('filesize'))
+
+                for i in range(len(path)):
+                    dirpath = '/'.join(path[:i+1])
+                    directory_sizes.setdefault(dirpath, 0)
+                    directory_sizes[dirpath] += filesize
+
+total = sum(v for v in directory_sizes.values() if v <= max_size)
+print(f'The sum of directory sizes less than {max_size:,} is {total:,}.')
+
+"""
+The total disk space available to the filesystem is 70000000. 
+To run the update, you need unused space of at least 30000000. 
+You need to find a directory you can delete that will free up enough space to run the update.
+
+In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; 
+this means that the size of the unused space must currently be 21618835, 
+which isn't quite the 30000000 required by the update. 
+Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+
+To achieve this, you have the following options:
+
+Delete directory e, which would increase unused space by 584.
+Delete directory a, which would increase unused space by 94853.
+Delete directory d, which would increase unused space by 24933642.
+Delete directory /, which would increase unused space by 48381165.
+
+Directories e and a are both too small; deleting them would not free up enough space. 
+However, directories d and / are both big enough! 
+Between these, choose the smallest: d, increasing unused space by 24933642.
+
+Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. 
+What is the total size of that directory?
+"""
+print(directory_sizes)
+
+available, required_unused = 70000000, 30000000
+current_unused = available - (directory_sizes['/'])
+print(current_unused)
+
+min_dir = directory_sizes['/']
+for d, v in directory_sizes.items():
+    if current_unused + v > required_unused and v < min_dir:
+        min_dir = v
+
+print(min_dir)
+print(current_unused + min_dir > required_unused)
---
+++ day08_treetop_tree_house.py
@@ -0,0 +1,150 @@
+"""
+30373
+25512
+65332
+33549
+35390
+
+Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
+
+A tree is visible if all the other trees between it and an edge of the grid are shorter than it.
+Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
+
+All the trees around the edge of the grid are visible -
+since they are already on the edge, there are no trees to block the view.
+In this example, that only leaves the interior nine trees to consider:
+
+The top-left 5 is visible from the left and top.
+    (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+The top-middle 5 is visible from the top and right.
+The top-right 1 is not visible from any direction;
+    for it to be visible, there would need to only be trees of height 0 between it and an edge.
+The left-middle 5 is visible, but only from the right.
+The center 3 is not visible from any direction;
+    for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+The right-middle 3 is visible from the right.
+In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+
+With 16 trees visible on the edge and another 5 visible in the interior,
+ßa total of 21 trees are visible in this arrangement.
+
+Consider your map; how many trees are visible from outside the grid?
+"""
+from pprint import pprint
+import collections
+import itertools
+
+tree_tuple = collections.namedtuple('tree_tuple', ['row', 'column', 'height'])
+visible_set = set()
+
+with open('day08_input.txt', 'r') as f:
+    forest = collections.deque(
+        collections.deque(
+            tree_tuple(r, c, int(height))
+            for c, height in enumerate(line.strip())
+        )
+        for r, line in enumerate(f.readlines())
+    )
+
+top, bottom = forest.popleft(), forest.pop()
+visible_set.update(top)
+visible_set.update(bottom)
+top.pop()
+top.popleft()
+bottom.popleft()
+bottom.pop()
+left, right = collections.deque(line.popleft() for line in forest),\
+       collections.deque(line.pop() for line in forest)
+visible_set.update(left)
+visible_set.update(right)
+data = sorted(itertools.chain(*forest), key=lambda tree: tree.column)
+
+
+for edge, row in zip(left, forest):
+    highest = edge.height
+    for tree in row.copy():
+        if tree.height > highest:
+            highest = tree.height
+            row.remove(tree)
+            visible_set.add(tree)
+
+
+for edge, row in zip(right, forest):
+    highest = edge.height
+    row_from_right = collections.deque(reversed(row))
+    for tree in row_from_right:
+        if tree.height > highest:
+            highest = tree.height
+            row.remove(tree)
+            visible_set.add(tree)
+
+
+forest_rearranged =\
+    collections.deque(
+        collections.deque(g)
+        for _, g in itertools.groupby(data, key=lambda tree: tree.column)
+    )
+
+for edge, column in zip(top, forest_rearranged):
+    highest = edge.height
+    for tree in column.copy():
+        if tree.height > highest:
+            highest = tree.height
+            column.remove(tree)
+            visible_set.add(tree)
+
+
+for edge, column in zip(bottom, forest_rearranged):
+    highest = edge.height
+    column_from_bottom = collections.deque(reversed(column.copy()))
+    for tree in column_from_bottom:
+        if tree.height > highest:
+            highest = tree.height
+            column.remove(tree)
+            visible_set.add(tree)
+
+print(f'{len(visible_set):,} trees are visible from outside the grid.')
+
+"""
+To measure the viewing distance from a given tree, look up, down, left, and right from that tree; 
+stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration. 
+(If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+The Elves don't care about distant trees taller than those found by the rules above; 
+the proposed tree house has large eaves to keep it dry, 
+so they wouldn't be able to see higher than the tree house anyway.
+
+In the example above, consider the middle 5 in the second row:
+
+30373
+25512
+65332
+33549
+35390
+
+Looking up, its view is not blocked; it can see 1 tree (of height 3).
+Looking left, its view is blocked immediately; it can see only 1 tree (of height 5, right next to it).
+Looking right, its view is not blocked; it can see 2 trees.
+Looking down, its view is blocked eventually; it can see 2 trees 
+    (one of height 3, then the tree of height 5 that blocks its view).
+
+A tree's scenic score is found by multiplying together its viewing distance in each of the four directions. 
+For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+30373
+25512
+65332
+33549
+35390
+
+Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+Looking left, its view is not blocked; it can see 2 trees.
+Looking down, its view is also not blocked; it can see 1 tree.
+Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+
+This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+Consider each tree on your map. What is the highest scenic score possible for any tree?
+"""
