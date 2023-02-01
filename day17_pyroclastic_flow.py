1c5785dcd188262d9d74eb2b5e163ddac98db296
amikami102 <asakomikami@gmail.com>
2023-02-01 13:51:12 -0500

    attempt at part 2 with caching

--- day17_pyroclastic_flow.py
+++ day17_pyroclastic_flow.py
@@ -1,235 +1,145 @@
 """
-Day 17: Pyroclastic Flow
+-- Day 17: Pyroclastic Flow --
 
 Inspired by
-    (very short code) https://github.com/juanplopes/advent-of-code-2022/blob/6794122df32a857827e0c49871e848afe62cff18/day17.py
+    - (very short code) https://github.com/juanplopes/advent-of-code-2022/blob/6794122df32a857827e0c49871e848afe62cff18/day17.py
 
-    (for a state hashing that's more elegant thant u/juanplopes's) https://www.reddit.com/r/adventofcode/comments/znykq2/comment/j0oa5tu/?utm_source=share&utm_medium=web2x&context=3
+    - (for a state hashing that's more elegant thant u/juanplopes's) https://www.reddit.com/r/adventofcode/comments/znykq2/comment/j0oa5tu/?utm_source=share&utm_medium=web2x&context=3
+
+    - (the most eloquent that uses caching and simulates the rock falling as an overlap between the previous state of the tower
+    without the rock and the current state of the tower with the rock)
+    https://github.com/Kamik423/advent-of-code-2022/blob/main/17.py
+
+    - (on caching) https://antonz.org/functools-cache/
 """
-import collections
 from typing import Iterable, Iterator
+import collections
 import itertools
 import functools
 
-INITIAL_OFFSET_RIGHT: int = 2 + 1
-INITIAL_OFFSET_TOP: int = 3 + 1
+INITIAL_OFFSET_LEFT: int = 2
+INITIAL_OFFSET_TOP: int = 3
+CHAMBER_WIDTH: int = 7
 
-Point = collections.namedtuple('Point', ['x', 'y'])
+CUTOFF_LENGTH: int = 50  # arbitrarily chosen
 
 
-def consume(iterator: Iterator, n: int) -> None:
+def convert_to_int(shape: list[str]) -> list[int]:
     """
-    Advance the iterator n-steps ahead. If n is None, consume entirely.
+    Convert the string representation of the shape of the rock into an integer representation
+    of the bitarray of the rock as it first appears at the top of the tower.
+    e.g. '1111' becomes '0011110' becomes 0b0011110.
     """
-    next(itertools.islice(iterator, n, n), None)
+    return [
+        int(line.zfill(INITIAL_OFFSET_LEFT + len(line)).ljust(CHAMBER_WIDTH, '0'), 2)
+        for line in shape
+    ]
 
 
-class Rock:
-    """
-    A class object representing a rock.
+RIGHT: int = int('1'.zfill(8), 2)  # 0b00000001
+LEFT: int = int('1'.ljust(8, '0'), 2)  # 0b1000000
+BOTTOM: int = int('1' * 7, 2)  # 0b111111
+InnerChamber = tuple[int, ...]
+Rock = list[int, ...]
+OverlappingPiece = tuple[int, ...]
+StateHash = tuple[InnerChamber, int, int]
 
-    Attributes
-    ---
-        shape : str
-            description of the shape of the rock
-        points: frozenset[Point]
-            a set of coordinates representing the rock
-        current_loc: Iterable[Point]
-            the coordinates within the chamber that the rock currently takes up
-
-    Methods
-    ---
-        update_loc(dx: int, dy: int) -> None
-            update current_loc attribute by shifting the x coordinate of points by dx and y coordinate by dy
-    """
-
-    def __init__(self, shape: str, points: Iterable[Point]):
-        self.shape = shape
-        self.points: frozenset[Point] = frozenset(Point(*p) for p in points)
-        self.current_loc = []
-
-    def __repr__(self) -> str:
-        return f'Rock shaped like {self.shape} at {self.current_loc}'
-
-    def update_loc(self, dx: int, dy: int) -> None:
-        old_loc = list(self.points) if not self.current_loc else self.current_loc.copy()
-        self.current_loc = [Point(point.x + dx, point.y + dy) for point in old_loc]
-
-
-class Chamber:
-    """
-    A class object representing the chamber through which rocks are falling through.
-
-    Attributes
-    ---
-        WIDTH : int, fixed to 7
-        INITIAL_HEIGHT : int, fixed to 3
-        FLOOR: int, fixed to 0
-        JETS : Iterator[str]
-            an iterator that cycles through the jet pattern read from the input fi.
-
-        height : int
-            the height of the chamber, which will increase as more rocks pile on
-        occupied: set[Point]
-            a collection of points within the walls of the chamber that are occupied by rocks
-
-    Methods
-    ---
-        free(point: Point) -> bool
-            Return True if the point is not occupied by a rock, otherwise False.
-
-        move(rock: Rock, dx: int, dy: int) -> bool
-            Return True if the rock can move so that its current_loc coordinates are transposed by (dx, dy).
-
-        place(rock: Rock) -> None
-            Let the rock fall through the chamber until it can no longer move, and
-            update the rock's current loc and add them to Chamber's set of occupied points.
-
-
-    """
-
-    def __init__(self, jet_pattern: str):
-        self.WIDTH: int = 7
-        self.INITIAL_HEIGHT: int = 3
-        self.FLOOR: int = 0
-        self.JETS: Iterator[str] = itertools.cycle(jet_pattern)
-        self.current_jet: str = None
-        self.height: int = 0
-        self.occupied: set[Point] = set()
-
-    def free(self, point: Point) -> bool:
-        return (point not in self.occupied) and (point.y > self.FLOOR) and (0 < point.x < self.WIDTH + 1)
-
-    def move(self, rock: Rock, dx: int, dy: int) -> bool:
-        return all(
-            self.free(Point(coord.x + dx, coord.y + dy)) for coord in rock.current_loc.copy()
+ROCKS: list[Rock] = list(
+        map(
+            lambda list_of_str: convert_to_int(list_of_str),
+            [
+                ['1111'],  # '-' shape
+                ['010', '111', '010'],  # '+' shape
+                ['111', '001', '001'],  # reverse 'L' shape
+                ['1'] * 4,  # 'l' shape
+                ['11'] * 2  # 2x2 square shape
+            ]
         )
+    )
+print(ROCKS)
 
-    def place(self, rock: Rock) -> None:
-        dx, dy = 0, 0
-        while True:
-            self.current_jet = next(self.JETS)
-            optional_shift = dx + (1 if self.current_jet == '>' else - 1)
-            if self.move(rock, optional_shift, dy):
-                dx = optional_shift
-                # otherwise, do not change dx and do not exit the while-loop yet
-            optional_fall = dy - 1
-            if not self.move(rock, dx, optional_fall):
-                # exit the while-loop when you can't fall any further
-                break
-            else:
-                dy = optional_fall
-        rock.update_loc(dx, dy)
-        self.occupied.update(rock.current_loc)
-        self.height = max(self.height, max(point.y for point in self.occupied))
-
-    def check_for_pockets(self) -> int:
-        sealed: int = functools.reduce(lambda i, j: i | j, (1 << (i + 1) for i in range(self.WIDTH)))
-        top2rows: Iterable[Point] = [
-            Point(x + 1, y)
-            for (x, y) in itertools.product(range(self.WIDTH), (self.height, self.height - 1))
-        ]
-        occupied_x: int = functools.reduce(
-            lambda i, j: i | j,
-            (point.x for point in top2rows if self.free(point))
-        )
-        return occupied_x if occupied_x == sealed else 0
-
-
-def solve(n_rocks: int, use_state_hash: bool = False) -> int:
+@functools.lru_cache(maxsize=40)
+def construct_overlapping_piece(chamber: InnerChamber, rock_index: int) -> OverlappingPiece:
     """
-    Solve the problem by simulating n_rocks falling down the chamber.
-    Optionally use state hashing by setting use_state_hash argument as True.
-    Return the height of the chamber after n_rocks have fallen down.
+    Construct an overlapping piece whose bottom layer is the chamber itself but replaced by 0's,
+    an offset layer of three 0's, and then the next rock about to fall.
     """
-    cycles: dict[str, tuple[int, int]] = {}
+    return tuple([0b0000000] * (len(chamber) + INITIAL_OFFSET_TOP) + ROCKS[rock_index])
 
-    chamber: Chamber = Chamber(jets)
-    chamber.occupied.clear()
-    rocks: Iterator[dict] = itertools.cycle(rock_specs)
-    counter: Iterator[int] = itertools.count()
-    delta_heights = 0
+@functools.lru_cache(maxsize=40)
+def simulate_rock_move(chamber: InnerChamber, overlapping_piece: OverlappingPiece, shift_dir: str) -> tuple[bool, OverlappingPiece]:
+    """
+    Simulate the rock making a move which consists of a single lateral shift and a single drop.
+    Return whether the rock can keep moving and the result of the move by updating the shape of the overlapping piece.
+    """
+    keep_moving: bool = True
+    if shift_dir == '>':
+        # check whether the shift overlaps with the right wall of the chamber or occupied spaces in the inner chamber
+        if all(l >> 1 & RIGHT == 0 for l in overlapping_piece) and all(l >> 1 & cl == 0 for l, cl in zip(overlapping_piece, chamber)):
+            overlapping_piece = tuple((layer >> 1 for layer in overlapping_piece))
+            print('shifted right', overlapping_piece)
+    else:
+        # check whether the left shift overlaps with the left wall of the chamber or  occupied spaces in the inner chamber
+        if all(l >> 1 & LEFT == 0 for l in overlapping_piece) and all(l << 1 & cl == 0 for l, cl in zip(overlapping_piece, chamber)):
+            overlapping_piece = tuple((layer << 1 for layer in overlapping_piece))
+    # check whether drop overlaps with the bottom layer or occupied spaces in the chamber
+    if all(cl & l == 0 for (cl, l) in itertools.zip_longest((BOTTOM, *chamber), overlapping_piece, fillvalue=0)):
+        return keep_moving, overlapping_piece[1:]
+    else:
+        return not keep_moving, overlapping_piece
 
-    while next(counter) < n_rocks:
-        current_rock = Rock(**next(rocks))
-        current_rock.update_loc(INITIAL_OFFSET_RIGHT, INITIAL_OFFSET_TOP + chamber.height)
-        chamber.place(current_rock)
+@functools.lru_cache(maxsize=40)
+def simulate_fall(chamber: InnerChamber, rock_index: int, jet_index: int) -> tuple[StateHash, int]:
+    """
+    Simulate a rock falling down the chamber until it stops and return a tuple of StateHash and the height added
+    to the chamber by the rock fall.
+    """
+    overlapping_piece: OverlappingPiece = construct_overlapping_piece(chamber, rock_index)
+    can_move: bool = True
+    while can_move:
+        print(overlapping_piece)
+        can_move, overlapping_piece = simulate_rock_move(chamber, overlapping_piece, jets[jet_index])
+        jet_index = (jet_index + 1) % len(jets)
+    chamber_updated: InnerChamber = tuple(
+        (cl | l for (cl, l) in itertools.zip_longest(chamber, overlapping_piece, fillvalue=0))
+    )
+    next_rock_index = (rock_index + 1) % len(ROCKS)
+    return (
+               chamber_updated[-CUTOFF_LENGTH:],
+               next_rock_index,
+               jet_index
+           ), \
+           len(chamber_updated) - len(chamber)
 
-        if use_state_hash:
-            state = chamber.check_for_pockets()
-            if not state:
-                continue
-            else:
-                print(state)
-                hash_key: str = f'{current_rock}|{chamber.current_jet}| {state}'
-                if hash_key in cycles.keys():
-                    old_height, prev_count = cycles[hash_key]
+@functools.lru_cache(maxsize=40)
+def simulate_through_n_rocks(n_rocks: int, state: StateHash = None) -> tuple[StateHash, int]:
+    """
+    Simulate n_rocks falling through the chamber.
+    """
+    chamber_height = 0
 
-                    delta_steps = abs(n_rocks - prev_count)
-                    delta_heights += (chamber.height - old_height) * delta_steps
-
-                    consume(rocks, n=delta_steps)
-                    consume(chamber.JETS, n=delta_steps)
-                    consume(counter, n=delta_steps)
-
-                cycles[hash_key] = (chamber.height, n_rocks)
-
-    chamber.height += delta_heights
-    return chamber.height
-
-
-rock_specs: Iterable[dict] = [
-    {
-        'shape': '-',
-        'points': [Point(*p) for p in ((0, 0), (1, 0), (2, 0), (3, 0))]
-    },
-    {
-        'shape': '+',
-        'points': [Point(*p) for p in ((0, 1), (1, 2), (1, 1), (2, 1), (1, 0))]
-    },
-    {
-        'shape': '」',
-        'points': [Point(*p) for p in ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2))]
-    },
-    {
-        'shape': 'l',
-        'points': [Point(*p) for p in ((0, 0), (0, 1), (0, 2), (0, 3))]
-    },
-    {
-        'shape': 'square',
-        'points': [Point(*p) for p in ((0, 0), (1, 0), (0, 1), (1, 1))]
-    }
-]
+    for rock_index in range(n_rocks):
+        if state is None:
+            state: StateHash = tuple(((), rock_index, 0))
+        state, delta_heights = simulate_fall(*state)
+        chamber_height += delta_heights
+        print(rock_index, state, delta_heights)
+    return state, chamber_height
 
 
-with open('day17_input.txt', 'r') as f:
+
+
+with open('day17_test.txt', 'r') as f:
     jets = f.read().strip()
-
-
+print(jets[0])
 
 # part 1
-N = 2022
-print(solve(N), 'without state-hashing')
-# part 2
-N = 6000
-print(solve(N, True), 'with state-hashing')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+_, height = simulate_through_n_rocks(4)
+print(height)
+# construct_overlapping_piece.cache_clear()
+# simulate_through_n_rocks.cache_clear()
+# simulate_fall.cache_clear()
+# simulate_rock_move.cache_clear()
 
 
 
