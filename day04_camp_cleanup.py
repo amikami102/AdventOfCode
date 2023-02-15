b7874dda8ce73055b70ca91ecbd5a439a77a97c4
amikami102 <asakomikami@gmail.com>
2023-02-15 13:15:11 -0500

    Cleaned up day04 script and files

--- day04_camp_cleanup.py
+++ day04_camp_cleanup.py
@@ -1,94 +1,78 @@
 """
---- Day 4 camp cleanup ---
+-- Day 4 camp cleanup --
 
-For example, consider the following list of section assignment pairs:
-
-2-4,6-8
-2-3,4-5
-5-7,7-9
-2-8,3-7
-6-6,4-6
-2-6,4-8
-For the first few pairs, this list means:
-
-Within the first pair of Elves, the first Elf was assigned sections 2-4 (sections 2, 3, and 4),
-while the second Elf was assigned sections 6-8 (sections 6, 7, 8).
-The Elves in the second pair were each assigned two sections.
-The Elves in the third pair were each assigned three sections: one got sections 5, 6, and 7,
-while the other also got 7, plus 8 and 9.
-This example list uses single-digit section IDs to make it easier to draw;
-your actual list might contain larger numbers. Visually, these pairs of section assignments look like this:
-
-.234.....  2-4
-.....678.  6-8
-
-.23......  2-3
-...45....  4-5
-
-....567..  5-7
-......789  7-9
-
-.2345678.  2-8
-..34567..  3-7
-
-.....6...  6-6
-...456...  4-6
-
-.23456...  2-6
-...45678.  4-8
-Some of the pairs have noticed that one of their assignments fully contains the other.
-For example, 2-8 fully contains 3-7, and 6-6 is fully contained by 4-6.
-In pairs where one assignment fully contains the other,
-one Elf in the pair would be exclusively cleaning sections their partner will already be cleaning,
-so these seem like the most in need of reconsideration. In this example, there are 2 such pairs.
-
-In how many assignment pairs does one range fully contain the other?
+Usage example
+    Advent_of_Code_2022 $ python day04_camp_cleanup.py day04_test.txt day04_input.txt
 """
-counter = 0
-with open('day04_input.txt', 'r') as f:
-    for pair in f:
-        first, second = (
-            set(
-                range(
-                    int(elf.split('-')[0]),
-                    int(elf.split('-')[1]) + 1
+import sys
+import itertools
+from typing import *
+
+
+def parse(txt_filename: str) -> Iterable[tuple[int, ...]]:
+    """
+    Each line of the file is '{left1}-{right1},{left2}-{right2}'.
+    Yield each line as a 4-length tuple of integers.
+
+    >>> parsed = parse("1-99,2-98\n41-98,41-97")
+    >>> for line in parsed:
+    >>>     print(parsed)
+    (1, 99, 2, 98)
+    (41, 98, 41,97)
+    """
+    pairs = []
+    with open(txt_filename, 'r') as f:
+        for line in f:
+            pairs.append(
+                tuple(
+                    map(int,
+                        itertools.chain.from_iterable(
+                            map(
+                                lambda pair: tuple(pair.split('-')),
+                                line.strip('\n').split(',')
+                            )
+                        )
+                    )
                 )
             )
-            for elf in pair.split(',')
-        )
-        common = first.intersection(second)
-        if common == first or common == second:
-            counter += 1
+    return pairs
 
-print(f'{counter:,} assignment pairs have one range fully contain the other.')
 
-"""
-It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+def expand_assignments(*pair) -> tuple[set[int], set[int]]:
+    """
+    For each pair of elves, get the range of sections each elf covers and return
+    these ranges as sets of integers.
+    """
+    left1, right1, left2, right2 = pair
+    return set(range(left1, right1 + 1)), set(range(left2, right2 + 1))
 
-In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
 
-5-7,7-9 overlaps in a single section, 7.
-2-8,3-7 overlaps all of the sections 3 through 7.
-6-6,4-6 overlaps in a single section, 6.
-2-6,4-8 overlaps in sections 4, 5, and 6.
-So, in this example, the number of overlapping assignment pairs is 4.
+def solve_part1(pairs: Iterable[tuple[int,...]]):
+    """Count how many pairs have one elf's sections properly contain the other elf's sections"""
+    counter: int = 0
+    for pair in pairs:
+        first, second = expand_assignments(*pair)
+        counter += 1 if first.issubset(second) or second.issubset(first) else 0
+    return counter
 
-In how many assignment pairs do the ranges overlap?
-"""
-counter = 0
-with open('day04_input.txt', 'r') as f:
-    for pair in f:
-        first, second = (
-            set(
-                range(
-                    int(elf.split('-')[0]),
-                    int(elf.split('-')[1]) + 1
-                )
-            )
-            for elf in pair.split(',')
-        )
-        common = first.intersection(second)
-        if common != set():
-            counter += 1
 
-print(f'{counter:,} assignments pairs have ranges overlapping.')
+def solve_part2(pairs: Iterable[tuple[int, ...]]):
+    """Count how many pairs have any overlap at all"""
+    counter: int = 0
+    for pair in pairs:
+        first, second = expand_assignments(*pair)
+        counter += 1 if first.intersection(second) != set() else 0
+    return counter
+
+
+if __name__ == '__main__':
+    title = 'Day 04: Camp Cleanup'
+    print(title.center(50, '-'))
+    for path in sys.argv[1:]:
+        data = parse(path)
+        part1 = solve_part1(data)
+        part2 = solve_part2(data)
+        print(f"""{path}:
+        Part 1: The number of pairs where one elf's assignment fully contains the other's is {part1}.
+        Part 2: The number of pairs where the assignments overlap is {part2}.
+        """)
