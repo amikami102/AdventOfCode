1cc75ec269835e30d40450857fabd903131514e7
amikami102 <asakomikami@gmail.com>
2023-02-16 11:56:38 -0500

    Cleaned up day06 scripts and files

--- day06_tuning_trouble.py
+++ day06_tuning_trouble.py
@@ -1,79 +1,53 @@
 """
-The device will send your subroutine a datastream buffer (your puzzle input);
-your subroutine needs to identify the first position
-where the four most recently received characters were all different.
-Specifically, it needs to report the number of characters
-from the beginning of the buffer
-to the end of the first such four-character marker.
-
-For example, suppose you receive the following datastream buffer:
-
-mjqjpqmgbljsphdztnvjfqwrcgsmlb
-
-After the first three characters (mjq) have been received,
-there haven't been enough characters received yet to find the marker.
-The first time a marker could occur is after the fourth character is received,
-making the most recent four characters mjqj. Because j is repeated, this isn't a marker.
-
-The first time a marker appears is after the seventh character arrives.
-Once it does, the last four characters received are jpqm, which are all different.
-In this case, your subroutine should report the value 7
-because the first start-of-packet marker is complete after 7 characters have been processed.
-
-Here are a few more examples:
-
-bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
-nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
-nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
-zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
-
-How many characters need to be processed before the first start-of-packet marker is detected?
+-- Day 06: Tuning Trouble --
 """
+import sys
+from typing import *
 import itertools
+import functools
 import collections
+T = TypeVar('T')
 
-with open('day06_input.txt', 'r') as f:
-    line = f.readline()
-n_char = 4
-signal = iter(line)
-window = collections.deque(itertools.islice(signal, n_char), maxlen=n_char)
-counter = n_char
-if len(set(window)) == n_char:
-    print(counter)
-for s in signal:
-    counter += 1
-    window.append(s)
-    if len(set(window)) == n_char:
-        break
 
-print(f"The first marker appears after character {counter}.")
+def parse(txt_filename: str) -> str:
+    """Return the one line of strings in the txt file"""
+    with open(txt_filename, 'r') as f:
+        return f.readline()
 
-"""
-Your device's communication system is correctly detecting packets, 
-ut still isn't working. It looks like it also needs to look for messages.
 
-A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+def _solve(line: str, n_char: int = 4) -> int:
+    """
+    Iterate through the line and return the index of the first character
+    after the most recent n_char string characters that are all unique.
+    """
+    def sliding_window(iterable: Iterable[T], n: int) -> Iterator[collections.deque[T]]:
+        it = iter(iterable)
+        window = collections.deque(itertools.islice(it, n), maxlen=n)
+        for item in it:
+            window.append(item)
+            if len(tuple(window)) == n:
+                yield tuple(window)
+        yield tuple(window)
 
-Here are the first positions of start-of-message markers for all of the above examples:
+    windows = sliding_window(line, n_char)
 
-mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
-bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
-nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
-nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
-zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
-How many characters need to be processed before the first start-of-message marker is detected?
-"""
+    return next(itertools.dropwhile(
+        lambda entry: len(set(entry[1])) < n_char,
+        enumerate(sliding_window(line, n_char))
+    ))[0] + n_char + 1
 
-n_char = 14
-signal = iter(line)
-window = collections.deque(itertools.islice(signal, n_char), maxlen=n_char)
-counter = n_char
-if len(set(window)) == n_char:
-    print(counter)
-for s in signal:
-    counter += 1
-    window.append(s)
-    if len(set(window)) == n_char:
-        break
 
-print(f"The first start-of-message marker is detected after character {counter}.")
+solve_part1: Callable = functools.partial(_solve, n_char=4)
+solve_part2: Callable = functools.partial(_solve, n_char=14)
+
+if __name__ == '__main__':
+    title = 'Day 06: Tuning Trouble'
+    print(title.center(50, '-'))
+    for path in sys.argv[1:]:
+        data = parse(path)
+        part1 = solve_part1(data)
+        part2 = solve_part2(data)
+        print(f"""{path}:
+        Part 1: The number of characters that need to be processed before the first start-of-the-parket marker is detected is {part1}.
+        Part 2: The number of characters that need to be processed before the first start-of-the-message marker is detected is {part2}.
+        """)
