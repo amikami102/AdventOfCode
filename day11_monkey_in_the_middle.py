1a7e1475d8cf0f941e4d01feb5a9764c23d3973e
amikami102 <asakomikami@gmail.com>
2023-02-17 21:00:20 -0500

    Cleaned day11 script

--- day11_monkey_in_the_middle.py
+++ day11_monkey_in_the_middle.py
@@ -1,124 +1,124 @@
-import re
+"""
+-- Day 11: Monkey in the Middle --
+
+Usage example:
+    Advent_of_Code/year2022 $ python day11_monkey_in_the_middle.py day11_test.txt day11_input.txt
+
+
+Two lessons:
+    1. Taking the common multiple of the monkey's divisors keeps the computation manageable.
+    2. When you're changing the data of other items in the list while iterating through the list,
+    things get messed up. You can't keep track of the items just by taking their index in enumerate().
+    Hash their items out in a separate variable.
+"""
+import sys
 import collections
-from pprint import pprint
+import re
+import operator
 import functools
-from typing import Sequence
+import dataclasses
+import math
+from typing import *
 
-import numpy as np
-
-monkey_patterns = [
-    "Monkey (?P<id>\d+):",
-    "Starting items: (?P<starters>[\d,\s]+)",
-    "Operation: new = old (?P<operator>\W) (?P<predicate>[\d]+|old)",
-    "Test: divisible by (?P<divisor>\d+)",
-    "If true: throw to monkey (?P<true_monkey>\d+)",
-    "If false: throw to monkey (?P<false_monkey>\d+)"
-]
-part2 = True
-
-
-def parse_block(block: list[str]) -> dict:
-    return {
-        k: v
-        for line, pattern in zip(block, monkey_patterns)
-        for k, v in re.match(pattern, line).groupdict().items()
-    }
+T = TypeVar('T')
+MONKEY_PATTERN: str =\
+"""Monkey (.+):
+  Starting items: (.+)
+  Operation: new = old (.) (.+)
+  Test: divisible by (.+)
+    If true: throw to monkey (.+)
+    If false: throw to monkey (.+)"""
+OPERATORS: dict[str, Callable] = {
+    '+': operator.add,
+    '*': operator.mul
+}
+Result = collections.namedtuple('Result', ['monkey', 'items'])
+Formula = Callable[Sequence, tuple[Result, Result]]
 
 
-def manage_worry_level(part2: bool = False):
-    if part2:
-        def wrapper(holding: Sequence) -> Sequence:
-            return holding
-    else:
-        def wrapper(holding: Sequence) -> Sequence:
-            return np.floor(holding / 3).astype(int)
-
-    return wrapper
-
-
-def define_monkey(modifier: callable, **kwargs) -> callable:
-    """
-    Define the monkey's inspection formula.
-    """
-    result = collections.namedtuple('result', ['to', 'items'])
-
-    def formula(holding: Sequence) -> tuple[result, result]:
-        holding = np.array(holding)
-
-        if kwargs['operator'] == '*' and kwargs['predicate'] == 'old':
-            holding = np.where(holding == 0, 0, holding ** 2)
-        elif kwargs['operator'] == '*':
-            holding = holding * int(kwargs['predicate'])
-        elif kwargs['operator'] == '+':
-            holding = holding + int(kwargs['predicate'])
-        else:
-            raise ValueError(f'unexpected operator {kwargs["operator"]}')
-
-        holding = modifier(holding)
-        test_true = int(kwargs['true_monkey']), np.ma.masked_where(
-            holding % int(kwargs['divisor']) != 0,
-            holding
-        ).compressed()
-        test_false = int(kwargs['false_monkey']), np.ma.masked_where(
-            holding % int(kwargs['divisor']) == 0,
-            holding
-        ).compressed()
-
-        return result(*test_true), result(*test_false)
-
-    return formula
-
-
+@dataclasses.dataclass
 class Monkey:
+    id: int
+    holding: collections.deque
+    operator: Callable
+    arg: str
+    divisor: int
+    true_monkey: int
+    false_monkey: int
 
-    def __init__(self, modifier: callable, **kwargs):
-        self.holding = collections.deque([])
-        self.inspected = collections.deque([])
-        self.holding.extend(
-            [int(i) for i in kwargs['starters'].split(', ')]
+
+def parse(txt_filename: str) -> list[Monkey]:
+    """
+    Read the monkey blocks and return as list of dictionaries containing the following keys: 'id', 'starters', 'operator', 'predicate', 'divisor', 'true_monkey', 'false_monkey'.
+    """
+
+    def parse_block(block: str) -> Monkey:
+        id, starters, operator, arg, divisor, true_monkey, false_monkey = \
+            tuple(*re.findall(MONKEY_PATTERN, block))
+        return Monkey(
+            id=int(id),
+            holding=collections.deque(map(int, starters.split(','))),
+            operator=OPERATORS[operator],
+            arg=arg,
+            divisor=int(divisor),
+            true_monkey=int(true_monkey),
+            false_monkey=int(false_monkey)
         )
-        self.recipe = define_monkey(modifier, **kwargs)
 
-    @property
-    def inspect(self) -> tuple:
-        self.inspected.extend(self.holding)
-        return self.recipe(self.holding)
-
-    def receive(self, items: list[int]):
-        self.holding.extend(items)
+    with open(txt_filename, 'r') as f:
+        return [
+            parse_block(block)
+            for block in f.read().split('\n\n')
+        ]
 
 
-with open('day11_input.txt', 'r') as f:
-    monkey_recipes = [
-        parse_block([line.strip() for line in block.split('\n')])
-        for block in f.read().split('\n\n')
-    ]
+def _simulate(monkeys: list[Monkey], n: int, relief: int = 1) -> int:
+    """
+    Simulate monkeys passing around the items for n rounds.
+    Optionally specify a modifier function to add to monkey formula.
+    Return the level of monkey business, which is computed
+    by multiplying the top two largest number of items inspected by the monkeys.
 
-mod_all = functools.reduce(
-    lambda prod, x: prod * x,
-    [int(recipe['divisor']) for recipe in monkey_recipes]
-)
-monkeys = [
-    Monkey(
-        manage_worry_level(part2),
-        **recipe
-    ) for recipe in monkey_recipes
-]
+    To make computation faster, each monkey's item is reduced to the modulo of mod_all, which is the least common multiple of all the monkey's divisors.
+    e.g. If monkey 1's divisor is 3 and monkey 2's divisor is 5, then item value 66 will give the same test results as mod(66, 15) or, equivalently, 6.
+    """
+    mod_all: int = functools.reduce(
+        lambda a, b: a * b,
+        (monkey.divisor for monkey in monkeys)
+    )
+    inspected: collections.Counter[int, int] = collections.Counter()
+
+    # This hashing gives the correct answers.
+    items: dict[int, collections.deque] = {
+        monkey.id: monkey.holding.copy()
+        for monkey in monkeys
+    }
+    for _ in range(n):
+        for monkey in monkeys:
+            inspected[monkey.id] += len(items[monkey.id])
+            for item in items[monkey.id]:
+                arg = item if monkey.arg == 'old' else int(monkey.arg)
+                new_item = (monkey.operator(item, arg) % mod_all) // relief
+                throw_to = monkey.true_monkey if new_item % monkey.divisor == 0 else monkey.false_monkey
+                items[throw_to].append(new_item)
+            items[monkey.id].clear()
+
+    return math.prod(v for _, v in inspected.most_common(2))
 
 
-for _ in range(10000):
-    for monkey in monkeys:
-        true_result, false_result = monkey.inspect
-        monkey.holding.clear()
-        monkeys[true_result.to].receive(true_result.items.tolist())
-        monkeys[false_result.to].receive(false_result.items.tolist())
+solve_part1 = functools.partial(_simulate, n=20, relief=3)
+solve_part2 = functools.partial(_simulate, n=10000, relief=1)
 
-for monkey in monkeys:
-    print(monkey.holding)
 
-tally = [
-    len(monkey.inspected) for monkey in monkeys
-]
-pprint(tally)
-tally.sort(reverse=True)
-print(f'The level of monkey business after 20 rounds is {tally[0] * tally[1]}.')
+if __name__ == '__main__':
+    title = 'Day 11: Monkey in the Middle'
+    print(title.center(50, '-'))
+
+    for path in sys.argv[1:]:
+        data = parse(path)
+        part1 = solve_part1(data)
+        part2 = solve_part2(data)
+        print(f"""{path}:
+        Part 1: The level of monkey business after 20 rounds is {part1}.
+        Part 2: The level of monkey business after 10000 rounds is {part2}.
+        """)
