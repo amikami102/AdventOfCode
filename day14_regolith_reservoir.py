3c05b5318f7b5e81405b1438b7fb600bef454244
amikami102 <asakomikami@gmail.com>
2023-02-20 16:43:02 -0500

    Cleaned day14 script

--- day14_regolith_reservoir.py
+++ day14_regolith_reservoir.py
@@ -1,213 +1,120 @@
 """
---- Day 14: Regolith Reservoir ---
+-- Day 14: Regolith Reservoir --
+
+Usage example:
+    Advent_of_Code/year2022 $ python day14_regolith_reservoir.py day14_test.txt day14_input.txt
+
+A tricky aspect of grids in Advent of Code is that sometimes the navigation is physical, (row, column), while others it's Cartesian, (x, y), which is what Day 14 uses. However, the y is inverted axis where y increases as you go "down" instead of "up."
 """
-import itertools
+import sys
+from typing import *
 import collections
-from typing import Iterable, Iterator, Tuple
+import itertools
+import pathlib
 import enum
 
-import numpy as np
+Coord = tuple[int, int]     # (x, y), not (row, column)
+Vector = Coord
+RockPath = list[Coord]
+
+DIRECTIONS = Down, DownLeft, DownRight = (0, 1), (-1, 1), (1, 1)
+Source = (500, 0)
+ROCK, SAND = '#', 'o'
 
 
-Point = collections.namedtuple('Point', ['x', 'y'])
+def _add_vectors(vector0: Vector, vector1: Vector) -> Vector:
+    return vector0[0] + vector1[0], vector0[1] + vector1[1]
 
 
-def draw_segment(start: Point, end: Point) -> tuple:
-    """
-    Draw a line segment from start to end by returning the
-    arrays of x indices and y indices.
-    For example, draw_segment(point(0,0), point(2,0)) would return
-    (array([0, 1]), array([0, 0])) because the segment would pass through
-    points (0, 0), (1,0), and (2,0).
-    """
-    right = int((end.x - start.x) / abs(end.x - start.x)) if abs(end.x - start.x) > 0 else 0
-    down = int((end.y - start.y) / abs(end.y - start.y)) if abs(end.y - start.y) > 0 else 0
-    x_indices = np.array([x for x in range(start.x, end.x + right * 1, right)]) \
-        if right != 0 else \
-        np.array([start.x for _ in range(start.y, end.y + down * 1, down)])
-    y_indices = np.array([y for y in range(start.y, end.y + down * 1, down)]) \
-        if down != 0 else \
-        np.array([start.y for _ in range(start.x, end.x + right * 1, right)])
-    return x_indices, y_indices
+class Grid(dict):
 
+    def __init__(self, directions: Iterable[Vector], default: str = KeyError) -> None:
+        super().__init__()
+        self.default = default
+        self.directions = directions
 
-class Path:
-    """
-    A python class object holding a path.
-    """
-    def __init__(self, points: Iterator[Point] | Iterable[Point]):
-        self.segments = []
-        self.points = list(points) if isinstance(points, Iterator) else points
-
-    def __repr__(self):
-        rep = ' -> '.join(f"{p.x, p.y}" for p in self.points)
-        return f'Path through {rep}'
-
-    def draw_segments(self):
-        self.segments.clear()
-        for endpoints in itertools.pairwise(self.points):
-            self.segments.append(
-                draw_segment(*endpoints)
-            )
-
-
-class Cell(enum.IntEnum):
-    AIR = 0
-    ROCK = 1
-    SAND = 2
-
-
-class gridLocation:
-
-    def __init__(self, **kwargs):
-        self.x, self.y = None, None
-        self.row, self.column = None, None
-
-        if 'x' in kwargs and 'y' in kwargs:
-            self.x, self.y = kwargs['x'], kwargs['y']
-        if 'row' in kwargs and 'column' in kwargs:
-            self.row, self.column = kwargs['row'], kwargs['column']
-
-
-def minmax(iterable, **kwargs):
-    """
-    Return minimum and maximum of input iterable.
-    """
-    my_list = list(iterable)  # to iterate over iterators twice
-    MinMax = collections.namedtuple('minmax', ['min', 'max'])
-    if not my_list:
-        if 'default' in kwargs:
-            return (kwargs['default'], kwargs['default'])
+    def __missing__(self, key: Coord):
+        if self.default == KeyError:
+            raise KeyError(f'{key} out of grid index')
         else:
-            raise ValueError('minmax is an empty iterable')
-    else:
-        return MinMax(
-            min=min(my_list, key=kwargs.get('key', None)),
-            max=max(my_list, key=kwargs.get('key', None))
-        )
+            return self.default
 
 
-class GridMatrix:
+def parse(txt_filename: str) -> list[RockPath]:
     """
-    A class holding grid-matrix data and
-    sand unit movement.
+    Each line of the file is a path represented by tuple -> tuple -> ....
     """
-    def __init__(self, xmin: int, xmax: int, ymin: int, ymax: int):
-        self.xmin, self.xmax, self.ymin, self.ymax = xmin, xmax, ymin, ymax
-        self._grid = np.full(
-            shape=(
-                abs(ymax - ymin) + 1,
-                abs(xmax - xmin) + 1
-            ),
-            fill_value=Cell.AIR
-        )
-        self._source = gridLocation(
-            x=500, y=0,
-            row=0, column=500-self.xmin
-        )
-
-    def draw_bottom(self):
-        for j in range(self.xmax - self.xmin+1):
-            self._grid[self.ymax, j] = Cell.ROCK
-
-    def draw_paths(self, paths: list[Path]):
-        """
-        Map rock paths on the grid.
-        """
-        for path in paths:
-            for segment in path.segments:
-                x_indices, y_indices = segment
-                i_indices, j_indices = y_indices, x_indices - self.xmin
-                self._grid[i_indices, j_indices] = Cell.ROCK
-
-    def blocked(self, loc: gridLocation) -> bool:
-        """A location is blocked if its cell is not filled with AIR."""
-        return self._grid[loc.row, loc.column] != Cell.AIR
-
-    def sand_rest(self, loc: gridLocation):
-        """
-        Block the cell location on the grid with SAND.
-        """
-        self._grid[loc.row, loc.column] = Cell.SAND
-        if loc == self._source:
-            raise OverflowError
-
-    def sand_fall(self, current_loc: gridLocation = None):
-        """
-        Sand unit will fall into the next unblocked cell in the following order:
-        - the cell directly below,
-        - the cell to the lower left, or
-        - the cell to the lower right.
-        If all three locations are blocked, rest the sand unit.
-        """
-        if current_loc is None:
-            current_loc = self._source
-
-        direct_below = gridLocation(row=current_loc.row + 1, column=current_loc.column)
-        left_diag = gridLocation(row=current_loc.row + 1, column=current_loc.column - 1)
-        right_diag = gridLocation(row=current_loc.row + 1, column=current_loc.column + 1)
-
-        if not self.blocked(direct_below):
-            return self.sand_fall(direct_below)
-        elif not self.blocked(left_diag):
-            return self.sand_fall(left_diag)
-        elif not self.blocked(right_diag):
-            return self.sand_fall(right_diag)
-        else:
-            return self.sand_rest(current_loc)
-
-
-with open('day14_input.txt', 'r') as f:
-    paths = [
-        Path(
-            Point(
-                int(coord.split(',')[0]),
-                int(coord.split(',')[1])
-            )
-            for coord in line.strip().split(' -> ')
-        )
-        for line in f
+    return [
+        [
+            (int(p.split(',')[0]), int(p.split(',')[1]))
+            for p in line.split(' -> ')
+        ]
+        for line in pathlib.Path(txt_filename).read_text().splitlines()
     ]
-for path in paths:
-    path.draw_segments()
 
 
-def measure_necessary_gridsize(part2: bool = False) -> tuple[int, int, int, int]:
-    xmin, xmax = minmax(
-        map(lambda coord: coord.x, (point for path in paths for point in path.points))
-    )
-    ymin, ymax = minmax(
-        map(lambda coord: coord.y, (point for path in paths for point in path.points))
-    )
-    if part2:
-        return min(-1200, xmin), max(1200, xmax), min(0, ymin), max(0, ymax+2)
-    else:
-        return min(500, xmin), max(500, xmax), min(0, ymin), max(0, ymax)
+def _place_rocks(grid: Grid, paths: list[RockPath]) -> Grid:
+    """
+    Place the paths of rocks on the grid.
+    """
+    for path_of_rocks in paths:
+        for (row0, col0), (row1, col1) in itertools.pairwise(path_of_rocks):
+            row_endpoints, col_endpoints = (row0, row1), (col0, col1)
+            for coord in itertools.product(
+                    range(min(row_endpoints), max(row_endpoints) + 1),
+                    range(min(col_endpoints), max(col_endpoints) + 1)
+            ):
+                grid[coord] = ROCK
+    return grid
 
 
-# part 1
-g = GridMatrix(*measure_necessary_gridsize())
-g.draw_paths(paths)
-for count in itertools.count(start=1):
-    try:
-        g.sand_fall()
-    except IndexError:
-        break
-print(count)
+def _simulate(grid: Grid, lay_floor: bool = False):
+    """
+    Simulate sand falling from Source until it hits the bottom floor of the grid
+    """
+    bottom = max(map(lambda coord: coord[1], grid))
+    if lay_floor:
+        bottom = bottom + 2
+        x_left, x_right = Source[0] - bottom, Source[0] + bottom
+        grid = _place_rocks(grid, [[(x_left, bottom), (x_right, bottom)]])
+    for count in itertools.count(start=1):
+        # particles enter from the Source
+        loc: Coord = Source
+        while True:
+            next_fall_locs = [
+                _add_vectors(loc, d)
+                for d in grid.directions if _add_vectors(loc, d) not in grid
+            ]
+            new_loc = next(iter(next_fall_locs), loc)
+            if new_loc == Source:   # block the source
+                return count
+            if new_loc == loc:
+                grid[new_loc] = SAND
+                break  # this particle has come to rest
+            elif new_loc[1] > bottom:
+                return count - 1    # stop counting before the particle falls into abyss
+            loc = new_loc
 
 
-# part 2
-g = GridMatrix(*measure_necessary_gridsize(True))
-g.draw_paths(paths)
-g.draw_bottom()
-for count in itertools.count(start=1):
-    try:
-        g.sand_fall()
-    except OverflowError:
-        break
-
-assert g._grid[g._source.row, g._source.column] == Cell.SAND
-print(count)
+def solve_part1(puzzle_input: list[RockPath]) -> int:
+    grid = _place_rocks(Grid(directions=DIRECTIONS), puzzle_input)
+    return _simulate(grid)
 
 
+def solve_part2(puzzle_input: list[RockPath]) -> int:
+    grid = _place_rocks(Grid(directions=DIRECTIONS), puzzle_input)
+    return _simulate(grid, lay_floor=True)
 
+
+if __name__ == '__main__':
+    title = 'Day 14: Regolith Reservoir'
+    print(title.center(50, '-'))
+
+    for path in sys.argv[1:]:
+        data = parse(path)
+        part1 = solve_part1(data)
+        part2 = solve_part2(data)
+        print(f"""{path}:
+        Part 1: The number of sand particles that come to rest until it starts to fall off into the abyss is {part1}.
+        Part 2: The number of sand particles that come to rest until the last one blocks the sand source is {part2}.
+        """)
