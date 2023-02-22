e2fdb04c9a6b789286adb3cb8d55f040d9a93ce5
amikami102 <asakomikami@gmail.com>
2023-02-22 16:55:50 -0500

    Cleaned day15 script

--- day15_beacon_exclusion_zone.py
+++ day15_beacon_exclusion_zone.py
@@ -1,195 +1,159 @@
 """
- Day 15: Beacon Exclusion Zone
+-- Day 15: Beacon Exclusion Zone --
+
+Usage example:
+    Advent_of_Code/year2022 $ python day15_beacon_exclusion_zone.py day15_test.txt day15_input.txt
+
+
+Inspired by Peter Norvig's solution:
+
+Part 1:
+Do not find all sensor detector ranges but limit the search within the target y coordinate region.
+
+Part 2:
+The main idea is illustrated by this reddit user's diagram. https://www.reddit.com/r/adventofcode/comments/zmcn64/comment/j0j289z/?utm_source=share&utm_medium=web2x&context=3
+The target beacon is located outside all sensors' ranges. Since this is the only such beacon, the beacon must lie
+    - above one sensor's upper right edge,
+    - another sensor's upper left edge,
+    - below a third sensor's lower right edge,
+    - and below a fourth sensor's lower left edge.
+So the steps to the solution are:
+    1. Draw points along the line segments 1 unit above/below detection edges of each sensor.
+    2. Filter out points whose x or y coordinate is outside [0, 4_000_000] or is detectable by a sensor.
+You should be left with one intersection point by the end of step 2.
+
+The above solution to part 2 takes longer than this solution, which involves more intermediate steps between 1 and 2.
+    A1. Draw the perimeter edges like step 1.
+    A2. Filter out edges that are not shared by at least 2 sensors.
+    A3. Find the intersection point for every pair of edges.
+    A4. Filter out intersection points whose x or y coordinate is outside [0, 4_000_000] or is detectable by some sensor.
 """
-from typing import Iterator, Iterable, NamedTuple
+import sys
+import pathlib
+import functools
 import re
 import collections
+import operator
 import itertools
+from typing import *
+
+Coord = tuple[int, int]     # (x, y)
+Vector = Coord
+SensorBeacon = collections.namedtuple('SensorBeacon', ['Sensor', 'Beacon'])
+
+INTEGER_PATTERN = re.compile('-?[0-9]+')
+X, Y = 0, 1
+DIAGONALS = NW, NE, SW, SE = (-1, -1), (1, -1), (-1, 1), (1, 1)
 
 
-Line = collections.namedtuple('Line', ['slope', 'intercept'])
-
-class Segment:
-    """A class holding a segment of consecutive integers."""
-    def __init__(self, *args):
-        self.start, self.stop = min(*args), max(*args)
-        self.width = abs(self.start - self.stop) + 1
-
-    def __repr__(self):
-        return f'Segment(start={self.start}, stop={self.stop})'
-
-    def overlaps(self, other) -> bool:
-        return self.stop >= other.start or self.start >= other.stop
-
-    def __lt__(self, other) -> bool:
-        return self.start < other.start
+def _add_vectors(vector1: Vector, vector2: Vector) -> Vector:
+    return vector1[X] + vector2[X], vector1[Y] + vector2[Y]
 
 
-line_pattern = re.compile(
-    'Sensor at x=(?P<x>-*\d+), y=(?P<y>-*\d+): closest beacon is at x=(?P<beacon_x>-*\d+), y=(?P<beacon_y>-*\d+)'
-)
-
-Beacon = collections.namedtuple('Beacon', ['x', 'y'])
+def _manhattan_distance(coord1: Coord, coord2: Coord) -> int:
+    return abs(coord1[X] - coord2[X]) + abs(coord1[Y] - coord2[Y])
 
 
-class Sensor:
+def parse(txt_filename: str) -> list[SensorBeacon]:
     """
-    A class representing a sensor.
+    Return each line as SensorBeacon class object.
+    e.g. "Sensor at x=155404, y=2736782: closest beacon is at x=2062250, y=2735130"
+    becomes `SensorBeacon(Sensor=(155404, 2736782), Beacon=(2062250, 2735130))`.
+    """
+    return [
+        SensorBeacon(
+            tuple(map(int, INTEGER_PATTERN.findall(line.split(':')[0]))),
+            tuple(map(int, INTEGER_PATTERN.findall(line.split(':')[1])))
+        )
+        for line in pathlib.Path(txt_filename).read_text().splitlines()
+    ]
 
-    Attributes:
-        line : str (string representation)
-        x, y: int, int (point coordinate of the sensor)
-        nearestBeacon: beacon (the nearest beacon detected by the sensor)
-        distance: int (the Manhattan distance between the nearest beacon and the sensor)
 
-    Methods:
-        sense_range(row: int) -> list[Segment]
-            Return a list of Segmentg containing the range of x's that the sensor can detect if the given row is
-            within the sensor's range, otherwise return None
-
-        detect_point(point: tuple[int, int]) -> bool
-            Return True if a given point is within the sensor's detection range, otherwise False
-
-        draw_boundary_lines(None) -> Iterator[Line]:
-            Yield the four Lines bounding the sensor's detection range
+def _count_empty_coords(pairs: list[SensorBeacon], yline: int = 10) -> int:
+    """
+    (Part 1)
+    Count known coordinates on y = y_line that we know cannot have a beacon.
+    Make sure to exclude any beacon located on y_line.
     """
 
-    def __init__(self, **kwargs):
-        self.x, self.y = int(kwargs['x']), int(kwargs['y'])
-        self.nearestBeacon = Beacon(x=int(kwargs['beacon_x']), y=int(kwargs['beacon_y']))
-        self.distance = abs(self.nearestBeacon.x - self.x) + abs(self.nearestBeacon.y - self.y)
+    def _mark_empty_coords(sensor: Coord, beacon: Coord) -> Iterator[int]:
+        """
+        Return a range of coordinates on Y = y_line that definitely do not have a beacon according to the sensor-beacon pair. Only return the X coordinate values.
+        """
+        radius: int = _manhattan_distance(sensor, beacon)
+        height: int = abs(yline - sensor[Y])
+        if height > radius:  # yline is outside of sensor radius
+            return range(0, 0)  # empty range
+        else:
+            x_left, x_right = sensor[X] - (radius - height), \
+                              sensor[X] + (radius - height)
+            return range(x_left, x_right + 1)  # return x from [x_left, x_right]
 
-    def __repr__(self):
-        return f'Sensor(x={x}, y={y})'
+    empty_coords = {x for pair in pairs for x in _mark_empty_coords(*pair)} \
+        - {beacon[X] for _, beacon in pairs if beacon[Y] == yline}
+    return len(empty_coords)
 
-    def sense_range(self, y: int) -> list[Segment]:
-        ranges = []
-        x_leg = self.distance - abs(self.y - y)
-        if x_leg > 0:
-            start, stop = self.x - x_leg, self.x + x_leg
-            if self.nearestBeacon.y == y:
-                if self.nearestBeacon.x == start:
-                    start += 1
-                else:
-                    stop -= 1
-            if self.y == y:
-                ranges.extend(
-                    [
-                        Segment(start, self.x - 1),
-                        Segment(self.x + 1, stop)
-                    ]
-                )
-            else:
-                ranges.append(Segment(start, stop))
-        return ranges
 
-    def detect_point(self, point: tuple[int, int]) -> bool:
-        x, y = point
-        return abs(self.x - x) + abs(self.y - y) <= self.distance
+def _find_distress_beacon(sb_pairs: list[SensorBeacon], max_value: int = 20):
+    """
+    (Part 2)
+    Find the distress beacon, which is between 0 and the value of y_max in both its x and y coordinate values AND is undetected by all the sensors.
+    Return the distress signal of the beacon.
+    """
 
-    def draw_boundary_lines(self) -> Iterator[Line]:
-        yield from (
-            Line(1, self.y - (self.x - self.distance - 1)),
-            Line(-1, self.y + (self.x - self.distance - 1)),
-            Line(1, self.y - (self.x + self.distance + 1)),
-            Line(-1, self.y + (self.x + self.distance + 1))
+    def _draw_above_below_sensor_edge(sensor: Coord, beacon: Coord) -> Iterator[Coord]:
+        """
+        Yield point drawn from the perimeter 1 unit around the sensor's detection range.
+        """
+        radius: int = _manhattan_distance(sensor, beacon) + 1
+        point: Coord = _add_vectors(sensor, (0, -radius))  # start at northernmost point
+        yield from itertools.accumulate(
+            (
+                r
+                for direction in (SE, SW, NW, NE)
+                for r in itertools.repeat(direction, radius)
+            ),
+            func=_add_vectors,
+            initial=point
         )
 
-
-def parse_line(line: str) -> dict:
-    """Parse line by applying regex matching and construct a Sensor object."""
-    matched = re.match(line_pattern, line)
-    return matched.groupdict()
-
-
-def merge_overlapping_segments(segment_list: Iterable[Segment]) -> Iterator[Segment]:
-    """
-    Given a list of Segments, merge any pair of line segments that overlap.
-    Return a list of non-overlapping segments.
-    """
-    head = None
-    for seg in sorted(segment_list):
-        if head is None:
-            head = seg
-        if head.overlaps(seg):
-            head = Segment(
-                min(head.start, seg.start),
-                max(head.stop, seg.stop)
+    def _disqualified(point: Coord) -> bool:
+        """
+        Return True if
+        - the point's x or y coordinate is outside [0, max_value]
+        - the point is within some sensor's detection range of a sensor.
+        """
+        return 0 > min(point) or\
+            max(point) > max_value or \
+            any(
+                _manhattan_distance(point, sensor) <= _manhattan_distance(sensor, beacon)
+                for sensor, beacon in sb_pairs
             )
-            continue
-        else:
-            yield head
-            head = seg
-    yield head
 
-
-def find_intersection(line1: Line, line2: Line) -> tuple[int, int] | None:
-    """
-    Find the intersection between two Lines.
-    If line1 and line2 have the same slope values, return None.
-    """
-    if line1.slope == line2.slope:
-        return None
-    else:
-        x = - (line1.intercept - line2.intercept) // (line1.slope - line2.slope)
-        y = line1.slope * x + line1.intercept
-        return x, y
-
-
-def sliding_window(iterable, n):
-    it = iter(iterable)
-    window = collections.deque(itertools.islice(it, n), maxlen=n)
-    if len(window) == n:
-        yield tuple(window)
-    for elem in it:
-        window.append(elem)
-        yield tuple(window)
-
-
-with open('day15_input.txt', 'r') as f:
-    sensors = [
-        Sensor(**parse_line(line)) for line in f
-    ]
-
-# part 1
-target_row = 2000000 #10
-sensible_ranges = itertools.chain(
-    *[
-        sensor.sense_range(target_row)
-        for sensor in sensors
-        if sensor.sense_range(target_row)
-    ]
-)
-merged = merge_overlapping_segments(sorted(sensible_ranges))
-known_empty = sum(abs(seg.start - seg.stop) + 1 for seg in merged)
-print(f'Row y={target_row} has {known_empty:,} positions that cannot contain a beacon.')
-
-
-# part 2
-lower, upper = 0, 4000000
-lines = collections.Counter(
-    itertools.chain(*(sensor.draw_boundary_lines() for sensor in sensors))
-)
-filtered = (
-    line for line, cnt in lines.items() if cnt >= 2
-)
-intersections = filter(
-    lambda pt: pt is not None and lower <= pt[0] <= upper and lower <= pt[1] <= upper,
-    (
-        find_intersection(*pair)
-        for pair in itertools.combinations(filtered, 2)
+    target_beacon = next(
+        p
+        for pair in sb_pairs
+        for p in _draw_above_below_sensor_edge(*pair)
+        if not _disqualified(p)
     )
-)
-
-for p in intersections:
-    if any(sensor.detect_point(p) for sensor in sensors):
-        continue
-    else:
-        tuning_freq = 4000000 * p[0] + p[1]
-        print(f'The only possible position for the distress beacon is at tuning frequency {tuning_freq}.')
-        break
+    return target_beacon[X] * 4_000_000 + target_beacon[Y]
 
 
+if __name__ == '__main__':
+    title = 'Day 15: Beacon Exclusion Zone'
+    print(title.center(50, '-'))
 
-
-
+    for path in sys.argv[1:]:
+        data = parse(path)
+        if 'test' in path:
+            solve_part1 = _count_empty_coords
+            solve_part2 = _find_distress_beacon
+        else:
+            solve_part1 = functools.partial(_count_empty_coords, yline=2_000_000)
+            solve_part2 = functools.partial(_find_distress_beacon, max_value=4_000_000)
+        part1 = solve_part1(data)
+        part2 = solve_part2(data)
+        print(f"""{path}:
+        Part 1: The number of positions that cannot contain a beacon is {part1}.
+        Part 2: The distress signal is {part2}.
+        """)
