ffaee31bb6d1ffc59c7803832af01ffecbfd172a
amikami102 <asakomikami@gmail.com>
2025-12-06 16:10:52 +0900

    year2024 changes

--- year2024/day18/main.py
+++ year2024/day18/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def parse(txtfile):
     pass
 
-def solve_part1(data):
+def solve_part1(data, **kwargs):
     pass
 
 def solve_part2(data):
@@ -14,15 +14,16 @@ def solve_part2(data):
 
 
 if __name__ == '__main__':
-    title = 'Day 5: Print Queue'
+    title = 'Day '
     print(title.center(50, '-'))
 
     for txtfile in sys.argv[1:]:
+        if txtfile == 'test.txt':
+            kwargs = {'width': 7, 'height': 7}
         data = parse(txtfile)
-        print(data)
-        part1 = solve_part1(data)
+        part1 = solve_part1(data, **kwargs)
         part2 = solve_part2(data)
         print(f"""[Data from {txtfile}]
-        Part 1: The number of XMASes is {part1}.
-        Part 2: The number of X-MASes is {part2}.
+        Part 1: The  is {part1}.
+        Part 2: The  is {part2}.
         """)
